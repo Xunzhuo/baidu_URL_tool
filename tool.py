1a7348f73e078bac20d10c933a8c3542c03b382b
xunzhuo <981242367@qq.com>
2020-04-17 11:18:02 +0800

    last

--- tool.py
+++ tool.py
@@ -7,42 +7,35 @@ import os
 import time
 import logging
 
-post_time = 1
-
 re_try = 50 # 服务器错误之后重新尝试的次数 默认50次
+sleep_time = 1 # 服务器错误后默认每隔1秒请求一次
 Your_website = "www.liuxunzhuo.tech" # the url record in your baidu_website
 Your_token = "ek5T1WJ6SXqZgoKU" # find it in your baidu_token
-sleep_time = 1 # 默认每隔1秒请求一次
+
 
 logging.basicConfig(level = logging.INFO, format = '%(asctime)s -%(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-while 1:	
+def Request():
 	response = os.popen("curl -H 'Content-Type:text/plain' --data-binary @urls.txt \"http://data.zz.baidu.com/urls?site="+Your_website+"&token="+Your_token+"\"").readlines()
-
-	logger.info("The "+str(post_time)+"`s request" + str(response))
+	logger.info("Request" + str(response))
 	if "over quota" in str(response):
-		logger.error("Today post max URLS , See You Tomorrow ~ ")
-		break
+		logger.error("Today post URLS , See You Tomorrow ~ ")
 	elif "empty content" in str(response):
-		logger.error("Make sure tool.py and url.txt in the same Directory ~ ")
-		break
+		logger.error("Make sure tool.py and url.txt in the same Directory and urls is right ~ ")
 	elif "only 2000 urls are allowed once" in str(response):
 		logger.error("Only 2000 urls are allowed once, resize your urls.txt ")
-		break
 	elif "site error"  in str(response):
 		logger.error("Y")
 	elif "401" or "404" in str(response):
 		logger.error("Token or Your_website is not valid , Check your Token  or Your_website")
-		break
 	elif "500" in str(response):
 		logger.warning("Server Problems , Automatically Re-Try")
-		if re_try > 0:
-			re_try = re_try - 1 
-			pass
-		else:
-			break
-	time.sleep(sleep_time)
-	post_time = post_time + 1
+		retry()
 
+def retry():
+	time.sleep(sleep_time)
+	Request()
+
+Request()
 logger.info("Finished")
